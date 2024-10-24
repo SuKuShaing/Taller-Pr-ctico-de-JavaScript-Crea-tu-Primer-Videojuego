da3be705a785ecf1671fd2d7ebcf70e06c4fe15f
Sebastian <sebastian.sanhueza.t@usach.cl>
2024-10-24 12:41:03 -0300

    Primeros pasos en el canvas

--- game.js
+++ game.js
@@ -1,1 +1,15 @@
-console.log(maps)
+const canvas = document.querySelector("#game");
+const game = canvas.getContext("2d");
+
+window.addEventListener("load", startGame);
+
+function startGame() {
+	game.fillRect(0, 100, 100, 100); // (donde parte en X, donde parte en y, hasta dónde en X 100 px [ancho], hasta dónde en y 100 px [largo])
+	game.clearRect(0, 0, 50, 50);
+	game.clearRect(50, 50, 10, 20);
+
+	game.font = "25px Verdana";
+	game.fillStyle = "purple";
+    game.textAlign = 'center'
+	game.fillText("Seba", 50, 50);
+}
